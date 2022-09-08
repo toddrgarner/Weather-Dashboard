91e9fd21a8070e5d2d31c14ed53bb0562cf74c9e
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-09-08 00:46:22 -0700

    updated index.html

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+//************************ Reference-Variables ************************//
+function initPage()
+var APIKey = "485c2c73dee04033790e901c450ffe0e";
+var inputEl = document.getElementById("city-input");
+var searchEl = document.getElementById("search-input");
+var clearEl = document.getElementById("clear-history");
+var nameEl = document.getElementById("city-name");
+var currentPixEl = document.getElementById("current-pix");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindeEl = document.getElementById("wind-speed");
+var currentUVEl = document.getElementById("UV-index");
+var historyEl = document.getElementById("history");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "austin" + "&appid=" + APIKey
+function start() {
+fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var latitude = data.coord.lat;
+        var longitude = data.coord.lon;
+        let cityID = data.id
+        let uvUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&cnt=1"
+    fetch(uvUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            
+            let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+            fetch(forecastUrl)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data)
+                })
+        })
+})
+    
+}
+
+
+start()
